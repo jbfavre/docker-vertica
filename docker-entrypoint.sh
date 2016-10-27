03492b22229834068d04a13845580d3003be9f4d
Jean Baptiste Favre <debian@jbfavre.org>
2016-10-27 17:28:52 +0200

    Update Dockerfile to remove useless gosu. Modify entrypoint script to allow better restart with persistent data

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,20 +4,42 @@ set -e
 # Vertica should be shut down properly
 function shut_down() {
   echo "Shutting Down"
-  gosu dbadmin /opt/vertica/bin/admintools -t stop_db -d docker -i
-  exit
+  vertica_proper_shutdown
+  echo 'Saving configuration'
+  mkdir -p ${VERTICADATA}/config
+  /bin/cp /opt/vertica/config/admintools.conf ${VERTICADATA}/config/admintools.conf
+}
+
+function vertica_proper_shutdown() {
+  echo 'Vertica: Closing active sessions'
+  /bin/su - dbadmin -c '/opt/vertica/bin/vsql -U dbadmin -d docker -c "SELECT CLOSE_ALL_SESSIONS();"'
+  echo 'Vertica: Flushing everything on disk'
+  /bin/su - dbadmin -c '/opt/vertica/bin/vsql -U dbadmin -d docker -c "SELECT MAKE_AHM_NOW();"'
+  echo 'Vertica: Stopping database'
+  /bin/su - dbadmin -c '/opt/vertica/bin/admintools -t stop_db -d docker -i'
+}
+
+function fix_filesystem_permissions() {
+  chown -R dbadmin:verticadba "${VERTICADATA}"
+  chown dbadmin:verticadba /opt/vertica/config/admintools.conf
 }
 
 trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
-chown -R dbadmin:verticadba "$VERTICADATA"
 
-if [ -z "$(ls -A "$VERTICADATA")" ]; then
-  echo "Creating database"
-  gosu dbadmin /opt/vertica/bin/admintools -t drop_db -d docker
-  gosu dbadmin /opt/vertica/bin/admintools -t create_db -s localhost -d docker -c /home/dbadmin/docker/catalog -D /home/dbadmin/docker/data
+echo 'Starting up'
+if [ -z "$(ls -A "${VERTICADATA}")" ]; then
+  echo 'Fixing filesystem permissions'
+  fix_filesystem_permissions
+  echo 'Creating database'
+  su - dbadmin -c "/opt/vertica/bin/admintools -t create_db --skip-fs-checks -s localhost -d docker -c ${VERTICADATA}/catalog -D ${VERTICADATA}/data"
 else
-  gosu dbadmin /opt/vertica/bin/admintools -t start_db -d docker -i
+  echo 'Restoring configuration'
+  cp ${VERTICADATA}/config/admintools.conf /opt/vertica/config/admintools.conf
+  echo 'Fixing filesystem permissions'
+  fix_filesystem_permissions
+  echo 'Starting Database'
+  su - dbadmin -c '/opt/vertica/bin/admintools -t start_db -d docker -i'
 fi
 
 echo "Vertica is now running"
